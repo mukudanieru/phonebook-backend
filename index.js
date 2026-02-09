df7ff29d238c5a2d98554f2176b892d33d197ac4
mukudanieru <111953203+mukudanieru@users.noreply.github.com>
2026-02-09 18:16:30 +0800

    chore: add ESLint configuration

--- index.js
+++ index.js
@@ -5,30 +5,6 @@ const Person = require("./models/person");
 
 const app = express();
 
-// TO BE REMOVED
-let persons = [
-  {
-    id: "1",
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: "2",
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: "3",
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: "4",
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
-
 const requestLogger = (request, response, next) => {
   console.log("Method:", request.method);
   console.log("Path:  ", request.path);
